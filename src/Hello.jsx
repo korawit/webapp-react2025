984df94390cec1dc3e2d6db0704b4c0ed2758e9f
korawit <korawit.orkphol@gmail.com>
2025-08-28 08:22:54 +0000

    Update title in index.html and add Hello component to render greetings

---
+++ src/Hello.jsx
@@ -0,0 +1,5 @@
+export default function Hello({name="somchai",surname="jaidee"}){
+    return (<>
+    <h1>Hello {name} {surname}</h1>
+    </>);
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Hello name="donald" surname="trump"/>
+    <Hello name="donald"/>
+    <Hello surname="trump"/>
   </React.StrictMode>
 );
 
